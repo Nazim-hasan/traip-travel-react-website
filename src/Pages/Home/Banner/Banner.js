4b7156c8b7f04b9470cbd4ccd4c883c5ef46e595
Nazim Hasan <nazimhasan124073@gmail.com>
2021-10-31 00:42:44 +0600

    all-done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -12,23 +12,16 @@ const Banner = () => {
                 />
                 <div className="custom-position">
                 <h4 className="text-success">from $890.00</h4>
-                <p>to 7 Days tour</p>
-                    <h1 className="text-dark fw-bolder">Welcome <span className="text-success">Traip</span></h1>
-                    <p>Feel sea breeze, salt in your hair and twittering birds</p>
+                <p>to 7 Days tour  
+                    <img className="img-icon" src="https://cdn-icons-png.flaticon.com/512/201/201623.png" alt="" />
+                </p>
+                    <h1 className="text-dark fw-bolder">Welcome <span className="text-success">Traip</span>
+                    <img className="img-icon" src="https://cdn-icons.flaticon.com/png/512/2988/premium/2988743.png?token=exp=1635619439~hmac=f38e8b48febb0a86ec74bac909c4fd1a" alt="" /></h1>
+                    <p>Feel sea breeze, salt in your hair and twittering birds
+                        <img className="img-icon" src="https://cdn-icons-png.flaticon.com/512/3069/3069186.png" alt="" />
+                    </p>
                 </div>
         </div>
-
-        // <div className="banner-area">
-        //     <img className="img-fluid" src={coverPhoto} alt="" />
-        //     <div className="custom-position">
-        //                     <h4 className="text-success">from $890.00</h4>
-        //                     <p>to 7 Days tour</p>
-        //                     <h1 className="">Welcome <span className="text-success">Traip</span></h1>
-        //                     <p>Feel sea breeze, salt in your hair and twittering birds</p>
-        //                     <button className="btn btn-success text-white">Read More</button>
-        //             {/* <NavLink to="/plans"><button className="btn btn-primary">See All Tour</button></NavLink> */}
-        //     </div>
-        // </div>
     );
 };
 
--- src/Pages/Home/HomeServices/HomeServices.js
+++ src/Pages/Home/HomeServices/HomeServices.js
@@ -13,7 +13,8 @@ const HomeServices = () => {
         <Container className="my-5">
             <div className="text-center">
             <h5 className="text-secondary">The Top Places For</h5>
-            <h1 className="text-success mb-5">Planning Your Holiday</h1>
+            <h1 className="text-success mb-5">Planning Your Holiday
+            <img className="img-icon" src="https://cdn-icons.flaticon.com/png/512/2627/premium/2627036.png?token=exp=1635620004~hmac=eacb4fd0991c02e66f21c65c43799d1d" alt="" /></h1>
             </div>
             <Row className="g-3">
                 {
